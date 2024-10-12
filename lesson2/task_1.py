a75677b7643bee23b59451a4fe4d96e73f658931
Gaallina <154439882+Gaallina@users.noreply.github.com>
2024-10-13 00:45:08 +0900

    2 урок

---
+++ lesson2/task_1.py
@@ -0,0 +1,5 @@
+lst = ['виноград', 'персик', 'груша', 'апельсин', 'банан', 'яблоко']
+grape = lst[0]
+print(grape)
+apple = lst[-1]
+print(apple)
---
+++ lesson2/task_9.py
@@ -0,0 +1,6 @@
+a = var_1 = 37
+b = var_2 = 99
+if (a == var_1) and (b == var_2):
+    a = var_2
+    b = var_1
+print('var_1 =', a,  'var_2 =', b)
